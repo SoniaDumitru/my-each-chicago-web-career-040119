4060b5cdcd2f8394523f95e30d591378f11b5209
Sonia Dumitru <sonya_dumitru@yahoo.com>
2019-02-21 22:53:43 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,9 @@
-def my_each(array)
-  i = 0
-  while i < array.length
-  yield array[i]
-  i = i + 1
-end
-array
-end
-
-
-#my_each(collection) do |i|
-#  puts i
-#end
+def my_each(array) # put argument(s) here
+  # code here
+     i = 0 
+     while i < array.length 
+     yield array[i]
+     i = i + 1 
+   end
+   array
+ end
